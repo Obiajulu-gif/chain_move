a9613bdf046ea5c0d6a489d674fcc7d48829a9aa
Damian <olebueziedamian@gmail.com>
2024-12-07 14:05:52 +0100

    added font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 // app/layout.js
 import "./globals.css";
-
+import { Montserrat } from "next/font/google";
 import LayoutHandler from "./LayoutHandler";
 import WalletProvider from "./WalletProvider";
 
@@ -11,11 +11,16 @@ export const metadata = {
     icon: "/images/blockridelogo.svg",
   },
 };
+const montserrat = Montserrat({
+  subsets: ["latin"], 
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-montserrat",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={montserrat.variable}>
+      <body className="font-montserrat">
         <WalletProvider>
           <LayoutHandler>{children}</LayoutHandler>
         </WalletProvider>
