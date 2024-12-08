9dae593e2fe651790a5674b54c07356adeff269b
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-12-08 07:52:24 +0100

    fixing changes

--- src/app/backend/DriverModel.js
+++ src/app/backend/DriverModel.js
@@ -1,14 +1,9 @@
 import mongoose from "mongoose";
 
-const DriverSchema = new mongoose.Schema(
-	{
-		fullName: { type: String, required: true },
-		idNumber: { type: String, required: true, unique: true },
-		document: { type: String, required: true }, // This can be a document URL or base64 string
-	},
-	{ timestamps: true }
-);
+const DriverSchema = new mongoose.Schema({
+	fullName: { type: String, required: true },
+	idNumber: { type: String, required: true },
+	document: { type: String, required: true },
+});
 
-const Driver = mongoose.models.Driver || mongoose.model("Driver", DriverSchema);
-
-export default Driver;
+export default mongoose.models.Driver || mongoose.model("Driver", DriverSchema);
